16f7fcdfe1bf269c3b5ca47cf040ce50f52cab5d
Tom Nason <tdn10@pitt.edu>
2013-04-04 00:04:15 -0400

    [TrainModel] Updated motion prototype - %grade
    Added track grade specification and relevant modifications to train
    motion

--- prototypes/TrainModel/TestTrainMotion.java
+++ prototypes/TrainModel/TestTrainMotion.java
@@ -5,8 +5,9 @@ public class TestTrainMotion {
 		
 
 		test.setPower(120000);
-		for(int i = 0; i < 1000000; i++) {
-			if(i % 1000 == 0 && i < 200000) {
+		test.setGrade(-1);
+		for(int i = 0; i < 10000000; i++) {
+			if(i % 10000 == 0 && i < 200000) {
 				test.printState();
 			}
 			test.motionStep();
@@ -15,8 +16,8 @@ public class TestTrainMotion {
 		test.setPower(0);
 		test.setEmergencyBrake(true);
 		
-		for(int i = 0; i < 10000; i++) {
-			if(i % 1000 == 0) {
+		for(int i = 0; i < 100000; i++) {
+			if(i % 10000 == 0) {
 				test.printState();
 			}
 			test.motionStep();
