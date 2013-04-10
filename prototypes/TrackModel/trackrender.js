a7dd5dc37bb02fb9901c85d8233b131b8aecf0d4
cnd11 <cnd11@pitt.edu>
2013-04-10 19:27:25 -0400

    organized

---
+++ prototypes/TrackModel/trackrender.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function TrackView(canvasID){
+	this.canvas = document.getElementById(canvasID);
+	this.context = canvas.getContext('2d');
+	
+	alert("foo");
+	alert(this.context);
+}
+
+
