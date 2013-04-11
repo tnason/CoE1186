d59d0cbfac5cbd3b691e8a581173a51bc99a1bfb
cnd11 <cnd11@pitt.edu>
2013-04-10 22:39:58 -0400

    necessary

---
+++ prototypes/build.sh
@@ -0,0 +1,5 @@
+javac -d . Environment/constData.java
+javac -d . Environment/Message.java
+javac -d . Environment/Worker.java 
+javac -d . TrackContoller/TrackController.java
+javac -d . TrackModel/Node.java TrackModel/Block.java
