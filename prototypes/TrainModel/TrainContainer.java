b585761c406f37cf6f83de7577aecf1c27d2d4a0
Tom Nason <tdn10@pitt.edu>
2013-04-11 17:05:28 -0400

    added prints

--- prototypes/TrainModel/TrainContainer.java
+++ prototypes/TrainModel/TrainContainer.java
@@ -17,6 +17,9 @@ public class TrainContainer extends Worker implements Runnable, constData
 	private final double TIME_STEP = .1; //timestep (in s) for train motion integration (simulation time!)
 
 	private int timerTrigger = 1; //real-time value (in ms) for triggering motionStep() calls
+
+	private int motionStepCount = 0;
+
 	//For now, simulationSpeedup = (trainTimestep * 1000) / timerTrigger
 
 	//for message sending/receiving
@@ -54,8 +57,13 @@ public class TrainContainer extends Worker implements Runnable, constData
 				else
 				{
 					tm.motionStep(); //move the trains!
+					if(motionStepCount % 10 == 0)
+					{
+						tm.printState();
+					}
 				}
 	  		}
+			motionStepCount++;
   		}
 	}
 
