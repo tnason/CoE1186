01fb63865701ff84901fe46bc5697bd88e1c84c0
jamesclampffer <clampfferj@gmail.com>
2013-04-11 18:37:08 -0300

    Update YardNode.java

--- prototypes/TrackModel/YardNode.java
+++ prototypes/TrackModel/YardNode.java
@@ -11,4 +11,17 @@ public class YardNode extends Node
 	public String toString(){
 		return "Yard" + super.toString();	
 	}
+	public Block getNextBlock(Block currentBlock) throws Exception{
+		if(input != null)
+		{
+			return input;
+		}
+		else if(output != null)
+		{
+			return output;
+		}
+		else{
+			throw new Exception("The yard node isnt connected to anything");
+		}
+	}
 }
