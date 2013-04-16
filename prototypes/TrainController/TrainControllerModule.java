7cc44b302cef6d171c77fc9328335004ddd36a2a
benkisley <benkisley@gmail.com>
2013-04-16 11:52:11 -0300

    Added all methods I need. Hopefully it compiles.

--- prototypes/TrainController/TrainControllerModule.java
+++ prototypes/TrainController/TrainControllerModule.java
@@ -8,7 +8,8 @@ public class TrainControllerModule extends Worker implements Runnable, constData
   
   private int trainID;
   private TrainController tc;
-  private TrainContainer trainModel;
+  private TrainContainer trainContainer;
+  private TrainModel tm;
   
   public TrainControllerModule()
   {
@@ -16,7 +17,7 @@ public class TrainControllerModule extends Worker implements Runnable, constData
     name = Module.trainController;
     msgs = new java.util.concurrent.LinkedBlockingQueue<Message>();
   }
-
+  
   public void run()
   {
     while(true)
@@ -34,49 +35,59 @@ public class TrainControllerModule extends Worker implements Runnable, constData
             if (controllers.containsKey(trainID))
             {
               tc = controllers.get(trainID); // Local TrainController
+  		  tm = trainContainer.getTrain(trainID); // Local TrainModel
             }
             switch (m.getType())
             {
+				case Sat_TnCnt_Request_Traversed_Block_Stats: //???????????
+					
+				break;
+				case Sat_TnCt_ReceiptConfirm_Traversed_Block_Stats: //??????????
+				
+				break;
               case MBO_TnCt_Send_Moving_Block_Authority: // Moving block authority from CTC
-                tc.movingBlockAuth = (Double)(m.getData().get("authority"));
+                tc.movingBlockAuth = (double)(m.getData().get("authority"));
                 sendPower();
                 break;
               case TcCt_TnCt_Send_Fixed_Block_Authority: // Fixed block authority from track controller
-                tc.fixedBlockAuth = (Double)(m.getData().get("authority"));
+                tc.fixedBlockAuth = (double)(m.getData().get("authority"));
                 sendPower();
                 break;
+				case TcMd_TnCt_Confirm_Occupancy_Return_Block_Stats: //????????????
+				
+				break;
+				case TcMd_TnCt_Confirm_Depopulation: //???????????
+				
+				break;	
               case CTC_TnCt_Send_Manual_FixedBlock: // Manual fixed block from CTC
-                tc.ctcFixedBlockAuth = (Double)(m.getData().get("authority"));
+                tc.ctcFixedBlockAuth = (double)(m.getData().get("authority"));
                 sendPower();
                 break;
               case CTC_TnCt_Send_Manual_Speed: // Manual velocity from CTC
-                tc.ctcOperatorVelocity = (Double)(m.getData().get("velocity"));
+                tc.ctcOperatorVelocity = (double)(m.getData().get("velocity"));
                 sendPower();
                 break;
+				case TcMd_TnCt_Send_Track_Gnd_State: // Underground state from track model
+				tc.underground = (boolean)(m.getData().get("state");
+				tc.setLights();
+				break;
+				case TcMd_TnCt_Send_Station_Name: // Next station name from track model
+					tc.nextStation = (String)(m.getData().get("stationName");
+					tc.stationAnnounced = false;
+					tc.announceStation();
+				break;
               case TcMd_TnCt_Send_Track_Speed_Limit: // Track speed limit from track model
-                tc.trackLimit = (Double)(m.getData().get("speedLimit"));
-                sendPower();
-                break;
-              case TnMd_TnCt_Send_Train_Velocity: // Current train velocity from train model
-                tc.velocity = (double)(m.getData().get("velocity"));
-                System.out.println("Got velocity of " + tc.velocity + " from train model.");
+                tc.trackLimit = (double)(m.getData().get("speedLimit"));
                 sendPower();
                 break;
               case CTC_TnCt_Send_Manual_MovingBlock: // Manual moving block authority from CTC
-                tc.ctcMovingBlockAuth = (Double)(m.getData().get("authority"));
+                tc.ctcMovingBlockAuth = (double)(m.getData().get("authority"));
                 sendPower();
-                break;
-              case TnMd_TnCt_Request_Train_Controller_Creation: // Train controller creation
-                TrainController newTrainController = new TrainController(trainID, this);
-                controllers.put(trainID, newTrainController);
-                break;
-              case TnMd_TnCt_Request_Train_Controller_Destruction:
-                tc.closeGUI();
-                controllers.remove(trainID);
                 break;
               default:
                 
                 break;
+				// get station status from track model
             }
           }
           else
@@ -106,21 +117,29 @@ public class TrainControllerModule extends Worker implements Runnable, constData
     Environment.passMessage(m);
   }
   
-  private void requestVelocity(){
-    String[] keys = {"trainID"};
-    Object[] data = {trainID};
-    send(new Message(name, name, Module.trainModel, msg.TnCt_TnMd_Request_Train_Velocity, keys, data));
-  }
   
   public void sendPower(){
-    requestVelocity();
-    double powerCommand = tc.setPower();
-    String[] keys = {"trainID", "power"};
-    Object[] data = {trainID, powerCommand};
-    send(new Message(name, name, Module.trainModel, msg.TnCt_TnMd_Send_Power, keys, data));
+    tc.velocity = tm.getVelocity();
+    tm.setPower(tc.setPower());
   }
   
   public void init(TrainContainer t){
-    trainModel = t;
+    trainContainer = t;
   }
-}
+  
+  // Methods for TrainContainer
+  public TrainController getTrainController(int trainID){
+	return controllers.get(trainID);
+  }
+  
+  public TrainController createTrainController(int trainID){
+	TrainController newTrainController = new TrainController(trainID, this, trainContainer.getTrain(trainID));
+    controllers.put(trainID, newTrainController);
+	return newTrainController;
+  }
+  
+  public void destroyTrainController(int trainID){
+	// Todo: remove from dropdown list of trains in GUI here
+	controllers.remove(trainID);
+  }
+  }
