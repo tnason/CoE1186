f072045292565f423f0767e92fcdd30d84de71fb
mhayes2351 <mhayes2351@gmail.com>
2013-04-21 03:17:39 -0300

    Create TrainRoute.java

---
+++ prototypes/Scheduler/TrainRoute.java
@@ -0,0 +1,112 @@
+package TLTTC;
+
+public class TrainRoute
+{
+  private long entryTime;
+	private long traverseTime;
+	private Node previousNode;
+	private Node nextNode;
+	private Block block;
+	private double speed;
+/*
+	public TrainRoute()
+	{
+		this.entryTime = -1;
+		this.traverseTime = -1;
+		this.previousNode = null;
+		this.nextNode = null;
+		this.block = null;
+	}
+
+	public TrainRoute(long entryTime, Block block, Node previousNode)
+	{
+		this.entryTime = entryTime;
+		this.traverseTime = -1;
+		this.previousNode = previousNode;
+		this.nextNode = null;
+		this.block = block;
+	}
+*/
+	public TrainRoute(long entryTime, long traverseTime, double speed, Block block, Node previousNode, Node nextNode)
+	{
+		this.entryTime = entryTime;
+		this.traverseTime = traverseTime;
+		this.speed = speed;
+		this.previousNode = previousNode;
+		this.nextNode = nextNode;
+		this.block = block;
+	}
+
+	public void setEntryTime(long time)
+	{
+		this.entryTime = time;
+	}
+
+	public void setTraverseTime(long time)
+	{
+		this.traverseTime = time;
+	}
+
+	public void setSpeed(double speed)
+	{
+		this.speed = speed;
+	}
+
+	public void setPreviousNode(Node node)
+	{
+		this.previousNode = node;
+	}
+
+	public void setNextNode(Node node)
+	{
+		this.nextNode = node;
+	}
+
+	public void setBlock(Block block)
+	{
+		this.block = block;
+	}
+
+	public long getEntryTime()
+	{
+		return entryTime;
+	}
+
+	public long getTraverseTime()
+	{
+		return traverseTime;
+	}
+
+	public double getSpeed()
+	{
+		return speed;
+	}
+
+	public Node getPreviousNode()
+	{
+		return previousNode;
+	}
+
+	public Node getNextNode()
+	{
+		return nextNode;
+	}
+
+	public Block getBlock()
+	{
+		return block;
+	}
+
+	public String toString()
+	{
+		String s = "Block: " + block.toString();
+		s = s.concat("\nPrevious Node: " + previousNode.toString());
+		s = s.concat("\nNext Node: " + nextNode.toString());
+		s = s.concat("\nEntry Time: " + entryTime);
+		s = s.concat("\nTraverse Time: " + traverseTime);
+		s = s.concat("\nSpeed: " + speed);
+
+		return s;
+	}
+}
+	
