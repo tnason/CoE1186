3130bc70010604f97f358d00ee393e8154f4aec5
Ben Kisley <benkisley@gmail.com>
2013-04-21 22:45:57 -0300

    Added setters and authority speed limit

--- prototypes/TrainController/TrainController.java
+++ prototypes/TrainController/TrainController.java
@@ -1,58 +1,43 @@
 package TLTTC;
 
-public class TrainController{
-  public int trainID;
+public class TrainController
+{
+  private int trainID;
+  private TrainModel tm;
   
-  public boolean underground = false;
-  public boolean inStation = false;
-  public String nextStation = "";
-  public boolean stationAnnounced = false;
-  public boolean daytime = false; // True = day, False = night
-  public boolean doorsOpen = false;
-  public boolean lightsOn = false;
-  public boolean engineFail = false;
-  public boolean signalPickupFail = false;
-  public boolean brakeFail = false;
+  private boolean underground = false;
+  private boolean inStation = false;
+  private String nextStation = "";
+  private boolean daytime = false; // True = day, False = night
+  private boolean doorsOpen = false;
+  private boolean lightsOn = false;
+  private boolean engineFail = false;
+  private boolean signalPickupFail = false;
+  private boolean brakeFail = false;
   
-  public double KP = 5000; // Proportional gain
-  public double ek = 0; // Proportional error
-  public double histEk = 0; // Proportional error one time step back
+  private final double KP = 5000; // Proportional gain
+  private double ek = 0; // Proportional error
+  private final double T = 0.1; // Sample period of train model (0.1 seconds)
+  private final double KI = 1000; // Integral gain
+  private double uk = 0; // Integral error
+  private double power = 0; // Power of train
+  private final double trainMaxPower = 120000.0; // Maximum power of train (120 kW)
   
-  public double T = 0.1; // Sample period of train model (0.1 seconds)
-  public double KI = 1000; // Integral gain
-  public double uk = 0; // Integral error
-  public double histUk = 0; // Integral error one time step back
+  private double trainOperatorVelocity = 0; // Velocity sent from train operator
+  private double ctcOperatorVelocity = 0; // Velocity sent from CTC operator
+  private double velocity = 0; // Current velocity of train
+  private double trackLimit = 0; // Track's speed limit
+  private final double trainLimit = 19.4444; // Train's speed limit (70 km/hr = 19.44 m/s)
   
-  public double power = 0; // Power of train
-  public double histPower = 0; // Power command one step back
-  public double trainMaxPower = 120000.0; // Maximum power of train (120 kW)
+  private double fixedBlockAuth = 0; // Fixed block authority
+  private double ctcFixedBlockAuth = 0; // Fixed block authority sent from CTC operator
+  private double movingBlockAuth = 0; // Moving block authority sent from MBO
+  private double ctcMovingBlockAuth = 0; // Moving block authority sent from CTC operator
   
-  public double trainOperatorVelocity = 0; // Velocity sent from train operator
-  public double ctcOperatorVelocity = 0; // Velocity sent from CTC operator
-  public double velocity = 0; // Current velocity of train
-  public double trackLimit = 0; // Track's speed limit
-  public double trainLimit = 19.4444; // Train's speed limit (70 km/hr = 19.44 m/s)
   
-  public double fixedBlockAuth = 0; // Fixed block authority
-  public double ctcFixedBlockAuth = 1400; // Fixed block authority sent from CTC operator
-  public double movingBlockAuth = 1400; // Moving block authority sent from MBO
-  public double ctcMovingBlockAuth = 1400; // Moving block authority sent from CTC operator
-  public double authority = 0;  // Overall authority of train
-  public double histAuthority = 0; // Overall authority of train one time step back
-  
-  public double trackLength = 1400; // Track length
-  
-  public TrainControllerGUI gui; // GUI
-  public TrainControllerModule module;
-  public TrainModel tm;
-  
-  public TrainController(int id, TrainControllerModule mod, TrainModel t)
+  public TrainController(int id, TrainModel t)
   {
-    gui = new TrainControllerGUI(mod);
-    //gui.openGUI();
-    gui.setVisible(true);
     trainID = id;
-    module = mod;
     tm = t;
     // Todo: connect to GPS here
     
@@ -68,77 +53,147 @@ public class TrainController{
     movingBlockAuth = 1400;
   }
   
+  
   public double setPower() // this method is called whenever an authority or new speed limit is received
   {
     // get failure flags and update UI
-    // get time for UI 
-    if (engineFail || signalPickupFail || brakeFail){
-      return 0.0;
+    // get time for UI
+  
+    if (engineFail || signalPickupFail || brakeFail) // If train failure, stop train
+	{
+		tm.setPower(0.0);
     }
-    
-    //System.out.println("Current velocity = " + velocity + " m/s.");
-    //System.out.println("Current setpoint = " + trainOperatorVelocity + " m/s.");
-    trainOperatorVelocity = Math.min(trainOperatorVelocity, ctcOperatorVelocity); // Selects safer of two velocities.
-    if ((trainOperatorVelocity > trackLimit) || (trainOperatorVelocity > trainLimit)) // If the operator sends a dangerous velocity
-    {
-      trainOperatorVelocity = Math.min(trackLimit, trainLimit); // Set to next highest allowable velocity
-    }
-    authority = Math.min(fixedBlockAuth, Math.min(ctcFixedBlockAuth, Math.min(movingBlockAuth, ctcMovingBlockAuth))); // Selects safest authority
-    if (authority > trackLength){ // If authority is unsafe, set it to the last authority
-      authority = histAuthority;
-    }
-    
-    ek = trainOperatorVelocity - velocity; // kth sample of velocity error
-    if (histPower < trainMaxPower)
-    {
-      uk = histUk + (T/2)*(ek + histEk);
-    }
-    else
-    {
-      uk = histUk;
-    }
-    power = ((KP*ek)+(KI*uk));
-    
-    histAuthority = authority;
-    histPower = power;
-    histEk = ek;
-    histUk = uk;
-    //System.out.println("Power command of " + power + " Watts sent.");
-    return power;
+	else
+	{
+		velocity = tm.getVelocity();
+		double authority = Math.min(Math.min(fixedBlockAuth, ctcFixedBlockAuth), Math.min(movingBlockAuth, ctcMovingBlockAuth)); // Selects safest authority
+		// At max train acceleration on steepest slope:
+		// a = F/m = (.5 + g*sin(2.86) - .001*g*cos(2.86)) = .9794 m/s^2
+		// d = (vi)(t) + (1/2)(a)(t^2) = vi*5.281 + (1/2)*.9794*27.889
+		// where t = 5.281 s is the average time to enter a new block at max speed
+		double authorityVelocityLimit = ((authority - 13.657)/5.281);
+		
+		double velocitySetpoint = Math.max(trainOperatorVelocity, ctcOperatorVelocity); // Selects faster of two velocities.
+		if (velocitySetpoint > Math.min((trackLimit, trainLimit), authorityVelocityLimit)) // If the operator sends a dangerous velocity,
+		{
+		  velocitySetpoint = Math.min((trackLimit, trainLimit), authorityVelocityLimit); // set to next highest allowable velocity
+		}
+
+		if (power < trainMaxPower)
+		{
+		  uk = uk + (T/2)*(ek + (velocitySetpoint - velocity));
+		}
+		ek = velocitySetpoint - velocity; // kth sample of velocity error
+		power = ((KP*ek)+(KI*uk));
+		tm.setPower(power);
+	}
   }
   
   
-  public void setDoors(){ // this method is called every time the train enters a new block
+  public void setDoors() // this method is called every time the train enters a new block
+  {
     velocity = tm.getVelocity();
     // get door status from train model
     
-    if (velocity == 0 && inStation && !doorsOpen){
+    if (velocity == 0 && inStation && !doorsOpen)
+	{
       // open doors
     }
-    else if (velocity != 0 && doorsOpen){
+    else if (velocity != 0 && doorsOpen)
+	{
       // close doors
     }
   }
   
-  public void setLights(){ // this method is called every time the train enters a new block
+  public void setLights() // this method is called every time the train enters a new block
+  {
     // get time from train model and set daytime variable
     
-    if (!daytime || underground && !lightsOn){
+    if (!daytime || underground && !lightsOn)
+	{
       // turn on lights
       // change UI
     }
-    else if (daytime && !underground && lightsOn){
+    else if (daytime && !underground && lightsOn)
+	{
       // turn off lights
       // change UI
     }
   }
   
   
-  public void announceStation(){ // this method is called whenever a station name is sent to the train controller
-    if (!stationAnnounced){
+  public void announceStation() // this method is called whenever a station name is sent to the train controller
+  {
       // announce station on train model
       // update UI so that button cannot be pressed
-      stationAnnounced = true;
-    }
+  }
+  
+  
+  public void setMovingBlockAuth(double m)
+  {
+	movingBlockAuth = m;
+	sendPower();
+  }
+  
+  
+  public void setCtcMovingBlockAuth(double m)
+  {
+	ctcMovingBlockAuth = m;
+	sendPower();
+  }
+  
+  
+  public void setFixedBlockAuth(double f)
+  {
+	fixedBlockAuth = f;
+	sendPower();
+  }
+  
+  
+  public void setCtcFixedBlockAuth(double f)
+  {
+	ctcFixedBlockAuth = f;
+	sendPower();
+  }
+  
+  
+  public void setCtcOperatorVelocity(double v)
+  {
+	ctcOperatorVelocity = v;
+	sendPower();
+  }
+  
+  
+  public void setTrainOperatorVelocity(double v)
+  {
+	trainOperatorVelocity = v;
+	sendPower();
+  }
+ 
+  
+  public void setTrackLimit(double v)
+  {
+	trackLimit = v;
+	sendPower();
+  }
+  
+  
+  public void setUnderground(boolean u)
+  {
+	underground = u;
+	setLights();
+  }
+  
+  
+  public void setInStation(boolean i){
+	inStation = i;
+	setDoors();
+  }
+  
+  
+  public void setNextStation(String s)
+  {
+	nextStation = s;
+	announceStation();
   }
 }
