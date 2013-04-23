05bbbdec40e8e63e5b68039e032c29d8023c4a27
Ben Kisley <benkisley@gmail.com>
2013-04-22 22:42:31 -0300

    Added GUI

--- prototypes/TrainController/TrainControllerModule.java
+++ prototypes/TrainController/TrainControllerModule.java
@@ -18,6 +18,7 @@ public class TrainControllerModule extends Worker implements Runnable, constData
   {
     controllers = new Hashtable<Integer, TrainController>();
     msgs = new LinkedBlockingQueue<Message>();
+    TrainControllerGUI gui = new TrainControllerGUI(this);
   }
   
   
