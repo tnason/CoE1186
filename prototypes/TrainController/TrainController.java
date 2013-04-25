3972a0540e331261b277f63cb0618fa036bd8414
Ben Kisley <benkisley@gmail.com>
2013-04-25 19:02:04 -0300

    Announce stations

--- prototypes/TrainController/TrainController.java
+++ prototypes/TrainController/TrainController.java
@@ -197,23 +197,26 @@ public class TrainController
 
     public void announceStation(boolean automatic) // this method is called every time the train enters a new block or manually
     {
-        /*if (automatic && !oldNextStation.equals(nextStation)) // If a new station name is processed, automatically announce it
-    {
-        oldNextStation = nextStation;
-        VoiceManager voiceManager = VoiceManager.getInstance();
-        Voice speaker = voiceManager.getVoice("kevin16");
-        speaker.allocate();
-        speaker.speak("Next stop " + nextStation + " on " + trainID);
-        speaker.deallocate();
-    }
-    else if (!automatic) // If button is pressed on GUI, announce station name
-    {
-        VoiceManager voiceManager = VoiceManager.getInstance();
-        Voice speaker = voiceManager.getVoice("kevin16");
-        speaker.allocate();
-        speaker.speak("Next stop " + nextStation + " on " + trainID);
-        speaker.deallocate();
-    }*/
+        if (automatic && !oldNextStation.equals(nextStation)) // If a new station name is processed, automatically announce it
+        {
+            oldNextStation = nextStation;
+            /*
+            VoiceManager voiceManager = VoiceManager.getInstance();
+            Voice speaker = voiceManager.getVoice("kevin16");
+            speaker.allocate();
+            speaker.speak("Next stop " + nextStation + " on " + trainID);
+            speaker.deallocate();*/
+            System.out.println("Next station = " + nextStation);
+        }  
+        else if (!automatic) // If button is pressed on GUI, announce station name
+        {
+            /*VoiceManager voiceManager = VoiceManager.getInstance();
+            Voice speaker = voiceManager.getVoice("kevin16");
+            speaker.allocate();
+            speaker.speak("Next stop " + nextStation + " on " + trainID);
+            speaker.deallocate();*/
+            System.out.println("Next station = " + nextStation);
+        }
     }
 
     public void setMovingBlockAuth(double m)
