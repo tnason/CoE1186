3f0689b30e341e3f2200f25d01256a3afd0e4a12
cnd11 <cnd11@pitt.edu>
2013-04-25 10:49:10 -0400

    commented TC

--- prototypes/TrackContoller/TrackControllerView.java
+++ prototypes/TrackContoller/TrackControllerView.java
@@ -2,8 +2,8 @@
 * Author(s): Cameron Dashti
 * Updated: 25 – 4 – 2013
 * Purpose: This file sets up the gui for the track controller. It allows
-           the user to view all of the block under each contoller and preform
-           various test meachanisms on the track.
+*          the user to view all of the block under each contoller and preform
+*          various test meachanisms on the track.
 */
 
 package TLTTC;
@@ -341,6 +341,14 @@ public class TrackControllerView extends javax.swing.JFrame
         String tcString = (String) tcList.getValueAt(tcList.getSelectedRow(), 0);
         selectedController = Integer.parseInt(tcString.split(" ")[2]);
 
+        for(Block curBlock : controller.get(selectedController).values())
+        {
+            if(curBlock.isOccupiedNoMaintenance())
+            {
+                numTrains++;
+            }
+        }
+
         // Remove all of the old controller information
         while(controllerInfoModel.getRowCount() > 0)
         {
@@ -348,9 +356,8 @@ public class TrackControllerView extends javax.swing.JFrame
         }
 
         // Add new contoller statistics
-
         controllerInfoModel.addRow(new Object [] { controller.get(selectedController).size(),
-                                                   "num Trains 0"});
+                                                   "num Trains " + numTrains});
         refresh();  // refresh the rest of the tables
     } 
 
@@ -363,11 +370,17 @@ public class TrackControllerView extends javax.swing.JFrame
 
         userSelectedBlock = choosenBlockNum; // save block number globally
 
+        // If the selected block is not occupied, it can be put into maintenance
         if(!currentBlock.isOccupiedNoMaintenance())
         {
             putMaintenance.setEnabled(true);      
         }
+        else
+        {
+            putMaintenance.setEnabled(false);
+        }
 
+        // If the user selects a crossing, allow them to enable the crossing.
         if(currentBlock.isCrossing() && !currentBlock.getCrossing())
         {
             enableCrossing.setEnabled(true);
@@ -385,13 +398,16 @@ public class TrackControllerView extends javax.swing.JFrame
     }                                                    
 
     private void enableCrossing(java.awt.event.MouseEvent evt) 
-    {                                
+    {               
+        // Check that the current block is a crossing                 
        if(currentBlock.isCrossing() && enableCrossing.isEnabled())
        {
+            // Create a thread scheduler to disable crossing.
             ScheduledThreadPoolExecutor delayReset = new ScheduledThreadPoolExecutor(1);
 
-            currentBlock.setCrossing(true);
+            currentBlock.setCrossing(true); // Enable the block's crossing
 
+            // Schecdule a new job taht disables the crossing after 30 seconds.
             delayReset.schedule(new Runnable() 
                               {
                                 public void run()
@@ -403,10 +419,12 @@ public class TrackControllerView extends javax.swing.JFrame
        }
     }                               
 
+    // If the use elects to toggle maintenance of a block
     private void putMaintenanceClicked(java.awt.event.MouseEvent evt) 
     {    
       if(putMaintenance.isEnabled())
         {
+            // toggle the stat of the maintenance of the block, update the table.
             currentBlock.setMaintenance(!currentBlock.getMaintenance());
             tcProp.setValueAt((Object) currentBlock.isOccupied(), tcProp.getSelectedRow(), 2);
             tcProp.setValueAt((Object) currentBlock.getMaintenance(), tcProp.getSelectedRow(), 3);
@@ -415,36 +433,42 @@ public class TrackControllerView extends javax.swing.JFrame
 
     private void toggleSwitchClicked(java.awt.event.MouseEvent evt) 
     {                                     
-        // TODO add your handling code here:
+        // if switch can be toggled, 
         if(toggleSwitch.isEnabled())
         {
-            System.out.println("HERE switch");
+            //System.out.println("HERE switch");
         }
             
     }                                      
 
+    // User is able to load the PLC class file to the track controller
     private void loadProg(java.awt.event.MouseEvent evt) 
     {                          
-        // TODO add your handling code here:
-       String className = "";
+        String className = "";
+
         try
         {   
-            JFileChooser choosePLC = new JFileChooser();
-            int returnVal = choosePLC.showOpenDialog(null);
+            JFileChooser choosePLC = new JFileChooser();        // open file chooser to user
+            int returnVal = choosePLC.showOpenDialog(null);     // Get the users option
 
-            if(returnVal != JFileChooser.APPROVE_OPTION)
+            if(returnVal != JFileChooser.APPROVE_OPTION)        // if user canceled, return
             {
                 return;
             }
 
-            className = choosePLC.getSelectedFile().getName();
+            // get the name of the class from file chooser
+            className = choosePLC.getSelectedFile().getName(); 
 
+            // Load the class from the package.
             plcClass = Class.forName("TLTTC."+className.split("\\.")[0]);
-                                                                                       
+                                                   
+            // create a new instance of the PLC                                                                           
             plcObject = plcClass.newInstance();  
 
+            // check that the loaded class is indeed a PLC.
             boolean isPLC = (boolean) plcClass.getMethod("verifyPLC").invoke(plcObject);
 
+            // If invalid class file, display error
             if(!isPLC)
             {
                 JOptionPane.showMessageDialog(null, "This is not a valid PLC!");
@@ -453,36 +477,42 @@ public class TrackControllerView extends javax.swing.JFrame
         } 
         catch (ClassNotFoundException cnfe)
         {
+            // if the class is not found, display error. Return
             JOptionPane.showMessageDialog(null, className + " is not a valid class file!");
             return;
         }
         catch (Exception e)
         {
+            // If any other exception occurs display it to the user.
             JOptionPane.showMessageDialog(null, "This is not a valid PLC!\n"+ e.toString());
             return;
         }
         
-        plcLoaded = true;
-        loadPLC.setEnabled(false);
+        plcLoaded = true;               // if reached this point, PLC is loaded.
+        loadPLC.setEnabled(false);      // disable PLC load button.
     }       
 
     public void refresh()
     {
-        while(tcPropModel.getRowCount() > 0)
+        while(tcPropModel.getRowCount() > 0)        // remove all rows fro the controller properties
         {
             tcPropModel.removeRow(0);
         }
 
-        if(selectedController == -1)
+        if(selectedController == -1)               // no controller selected, return
         {
             return;
         }
 
+        // loop over all blocks in the current controller
         for(Block b : controller.get(selectedController).values())
         {
-            String crossing = (b.isCrossing()) ? (""+ b.getCrossing()) : "-";
+            // determine if block is a crossing
+            String crossing = (b.isCrossing()) ? (""+ b.getCrossing()) : "-"; 
+
             try
             {
+                // Try to add the block, assuming there is a next block
                 tcPropModel.addRow(new Object [] { b.getID(), 
                                                    b.getNextBlock(b.getStartNode()).getID(),
                                                    b.isOccupied(),
@@ -492,6 +522,7 @@ public class TrackControllerView extends javax.swing.JFrame
             }
             catch(Exception e)
             {
+                // if no next block, the block points to the yard
                 tcPropModel.addRow(new Object [] { b.getID(), 
                                                    "yard",
                                                    b.isOccupied(),
@@ -501,6 +532,7 @@ public class TrackControllerView extends javax.swing.JFrame
             }
         }
 
+        // disable all buttons until next block is selected.
         enableCrossing.setEnabled(false);
         putMaintenance.setEnabled(false);
         toggleSwitch.setEnabled(false);
