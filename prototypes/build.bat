10fd0c85a352fa882baa46fa85bbed78e2c30606
Ben Kisley <benkisley@gmail.com>
2013-04-25 18:57:28 -0300

    Added voice to speech

--- prototypes/build.bat
+++ prototypes/build.bat
@@ -1,2 +1,2 @@
-javac -d . CTCOffice/*.java Environment/*.java Satellite/*.java Scheduler/*.java TrackContoller/*.java TrackModel/*.java TrainController/*.java TrainModel/*.java
+javac -cp /freetts-1.2.2-bin/freetts-1.2/lib/freetts.jar -d . CTCOffice/*.java Environment/*.java Satellite/*.java Scheduler/*.java TrackContoller/*.java TrackModel/*.java TrainController/*.java TrainModel/*.java
              
